ee0d93c0479ac4840c3c8de05d436fa1d8ed123f
alin <alinoctavian@protonmail.com>
2020-09-10 12:56:44 +0300

    Added WPF

---
+++ Entry.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace F4MPLauncher
+{
+    /// <summary>
+    /// Interaction logic for Entry.xaml
+    /// </summary>
+    public partial class Entry : Window
+    {
+        public Entry()
+        {
+            InitializeComponent();
+        }
+    }
+}
